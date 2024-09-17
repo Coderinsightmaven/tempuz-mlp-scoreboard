63141d1eb738949a7c58568cdc68c8a1c2092e5c
Coderinsightmaven <lmarincik@proton.me>
2024-09-16 23:05:47 -0600

    added favicon custom title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import MLPScoreboard from '@/components/MLPScoreboard'
+
 export const runtime = "edge"
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Welcome</h1>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <MLPScoreboard />
     </main>
-  );
+  )
 }
