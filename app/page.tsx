7fb2ec597682340dc91092f6b04d864adcc2d550
Coderinsightmaven <lmarincik@proton.me>
2024-09-18 00:57:37 -0600

    removed edge line of code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import MLPScoreboard from '@/components/MLPScoreboard'
 
-export const runtime = "edge"
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
